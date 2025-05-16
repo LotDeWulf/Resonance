bc6d3f2a1527ff1ca9439e9ad561505d4096a854
LotDeWulf <lot.dewulf@gmail.com>
2025-05-16 11:38:39 +0200

    verandering tijd van 10 naar 5 seconden

--- emotion.py
+++ emotion.py
@@ -48,7 +48,7 @@ def detect_emotion():
         except Exception as e:
             print("Fout:", e)
 
-        time.sleep(10)
+        time.sleep(5)
 
 threading.Thread(target=detect_emotion, daemon=True).start()
 
